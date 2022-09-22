3b1c4e62723afa6eccd1a9b17d945b88f3608e96
Mehdi <112829615+Bixroww@users.noreply.github.com>
2022-09-22 22:15:35 +0200

    Add files via upload

---
+++ API/index.js
@@ -0,0 +1,8 @@
+const quote = document.getElementById('quote');
+
+fetch('https://api.quotable.io/random')
+    .then((response) => response.json())
+    .then(data => {
+        quote.innerHTML = data.content
+    })
+    
